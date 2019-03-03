20da619498b4ec6e58e78e81e37b84e4274816b7
jwierzbicki <jwierzbicki22@gmail.com>
2019-03-03 12:21:38 +0100

    Initial commit

---
+++ MainActivity.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace KitchenStatusClient
+{
+    [Activity(Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            SupportActionBar.Title = "My toolbar!";
+
+            //var toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+            //SetActionBar(toolbar);
+            //ActionBar.Title = "My Toolbar";
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return base.OnCreateOptionsMenu(menu);
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            Toast.MakeText(this, "Action selected: " + item.TitleFormatted, ToastLength.Short).Show();
+
+            return base.OnOptionsItemSelected(item);
+        }
+	}
+}
+
